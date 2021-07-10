7b22a2f202b51551ab66669c6fe8736a127d5fb1
Aleksandra <olapawelec13@gmail.com>
2021-07-11 01:45:20 +0200

    add html pages and js functionality

--- scripts/main.js
+++ scripts/main.js
@@ -6,31 +6,43 @@
 // }
 
 let myImage = document.querySelector('img');
+let myFigure = document.querySelector('.highlight-figure');
+let myButton = document.querySelector('.change-user-btn');
+let myHeading = document.querySelector('h1');
+let imgDesc = document.querySelector('#highlight-img-desc');
 
-myImage.onclick = function(){
+myImage.onclick = function () {
     let mySrc = myImage.getAttribute('src');
 
-    mySrc === 'img/splash.jpeg' ? myImage.setAttribute('src', 'img/ghost_info.jpeg') : myImage.setAttribute('src', 'img/splash.jpeg');
+    // mySrc === 'img/splash.jpeg' ? myImage.setAttribute('src', 'img/ghost_info.jpeg')  : myImage.setAttribute('src', 'img/splash.jpeg');
+
+    if (mySrc === 'img/splash.jpeg') {
+        myImage.setAttribute('src', 'img/ghost_info.jpeg');
+        imgDesc.innerHTML = 'Many ghosts are said to manifest during the night.';
+    } else {
+
+        myImage.setAttribute('src', 'img/splash.jpeg');
+        imgDesc.innerHTML = 'There are many uncharted phenomens in this world.';
+    }
 }
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
+
 
 function setUserName() {
     let myName = prompt('Please enter your name');
-    
-    if(!myName){
+
+    if (!myName) {
         setUserName();
 
-    }else{
+    } else {
         localStorage.setItem('name', myName);
         myHeading.textContent = `Paranormal creatures guide for ${myName}`;
     }
 }
 
-if(!localStorage.getItem('name')){
+if (!localStorage.getItem('name')) {
     setUserName();
-}else{
+} else {
     let storedName = localStorage.getItem('name');
     myHeading.innerHTML = `Paranormal creatures guide for ${storedName}`;
 }
@@ -38,5 +50,3 @@ if(!localStorage.getItem('name')){
 myButton.onclick = function () {
     setUserName();
 }
-
-setUserName();
